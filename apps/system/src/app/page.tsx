f9522275b7467edda8d2c8783e5699a2389683cf
Byron Wade <bcw1995@gmail.com>
2023-10-07 23:32:20 -0700

    test

--- apps/system/src/app/page.tsx
+++ apps/system/src/app/page.tsx
@@ -1,8 +1,25 @@
-import Link from "next/link";
-export default function Home() {
+"use client";
+import React from "react";
+import { Epg, Layout } from "planby";
+
+// Import hooks
+import { useApp } from "./useApp";
+
+// Import components
+import { Timeline, ChannelItem, ProgramItem } from "../components";
+
+function Home() {
+	const { isLoading, getEpgProps, getLayoutProps } = useApp();
+
 	return (
-		<main>
-			<Link href="/dashboard">Dashboard</Link>
-		</main>
+		<div>
+			<div style={{ height: "80vh", width: "100%" }}>
+				<Epg isLoading={isLoading} {...getEpgProps()}>
+					<Layout {...getLayoutProps()} renderTimeline={(props) => <Timeline {...props} />} renderProgram={({ program, ...rest }) => <ProgramItem key={program.data.id} program={program} {...rest} />} renderChannel={({ channel }) => <ChannelItem key={channel.uuid} channel={channel} />} />
+				</Epg>
+			</div>
+		</div>
 	);
 }
+
+export default Home;
